f2d777beb01b42faa61cdd8313b6a06d854dadfb
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-06-27 23:56:48 -0300

    add: guard de roles

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,14 +1,3 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './usuarios/pages/login/login.component';
-import { RegisterComponent } from './usuarios/pages/register/register.component';
 
-export const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-];
+export const routes: Routes = [];
