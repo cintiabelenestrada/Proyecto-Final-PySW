9caea0f28d795071748485b900e1852033566821
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-07-08 00:44:04 -0300

    routing alta locales

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./layouts/nav/nav.component";
 import { PropietarioComponent } from './propietario/propietario/propietario.component';
 import { FormPropietarioComponent } from './propietario/form-propietario/form-propietario.component';
+import { LocalComponent } from './locales/pages/local/local.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavComponent,PropietarioComponent,FormPropietarioComponent]
+    imports: [RouterOutlet, NavComponent,PropietarioComponent,FormPropietarioComponent, LocalComponent]
 })
 export class AppComponent {
   title = 'Frontend';
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PropietarioComponent } from './propietario/propietario/propietario.component';
 import { FormPropietarioComponent } from './propietario/form-propietario/form-propietario.component';
+import { LocalComponent } from './locales/pages/local/local.component';
 
 export const routes: Routes = [
 
 { path: 'propietario', component: PropietarioComponent},
-{ path: 'form-propietario/:id', component: FormPropietarioComponent}
+{ path: 'form-propietario/:id', component: FormPropietarioComponent},
+{ path: 'local', component: LocalComponent},
 ];
