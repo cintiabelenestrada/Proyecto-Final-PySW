21efe6cdc63a985ae6b92a6fb38daaad6b466f24
ezealavar <ezequielalavar2@gmail.com>
2024-07-07 21:14:51 -0300

    Se agrega interesAnual como nuevo atributo de Alquiler

--- Backend/models/Alquiler.js
+++ Backend/models/Alquiler.js
@@ -4,9 +4,10 @@ const { Schema } = mongoose;
 const AlquilerSchema = new Schema({
     propietario: { type: Schema.Types.ObjectId, ref: 'Propietario', required: true },
     local: { type: Schema.Types.ObjectId, ref: 'Local', required: true },
-    ambientes: { type: Number, required: true },
+    plazoMes: { type: Number, required: true },
     costoAlquiler: { type: Number, required: true },
-    fechaAlquiler: { type: Date, required: true }
+    fechaAlquiler: { type: Date, default: Date.now },
+    interesAnual: { type: Number, required: true }
 });
 
 module.exports = mongoose.models.Alquiler || mongoose.model('Alquiler', AlquilerSchema);
