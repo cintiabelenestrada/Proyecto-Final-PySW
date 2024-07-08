34ff79b3c576a45d6d8d93b77820cf25ead98508
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-07-08 02:25:42 -0300

    routing locales

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { NavComponent } from "./layouts/nav/nav.component";
 import { PropietarioComponent } from './propietario/propietario/propietario.component';
 import { FormPropietarioComponent } from './propietario/form-propietario/form-propietario.component';
 import { LocalComponent } from './locales/pages/local/local.component';
+import { LocalesComponent } from './locales/pages/locales/locales.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavComponent,PropietarioComponent,FormPropietarioComponent, LocalComponent]
+    imports: [RouterOutlet, NavComponent,PropietarioComponent,FormPropietarioComponent, LocalComponent, LocalesComponent]
 })
 export class AppComponent {
   title = 'Frontend';
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { PropietarioComponent } from './propietario/propietario/propietario.component';
 import { FormPropietarioComponent } from './propietario/form-propietario/form-propietario.component';
 import { LocalComponent } from './locales/pages/local/local.component';
+import { LocalesComponent } from './locales/pages/locales/locales.component';
 
 export const routes: Routes = [
 
 { path: 'propietario', component: PropietarioComponent},
 { path: 'form-propietario/:id', component: FormPropietarioComponent},
 { path: 'local', component: LocalComponent},
+{ path: 'locales', component: LocalesComponent}
 ];
