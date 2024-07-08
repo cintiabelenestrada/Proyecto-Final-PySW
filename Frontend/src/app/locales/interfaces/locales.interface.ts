7551c551797ccdd91215794f4a7b40e008db3bdd
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-07-08 01:54:32 -0300

    interface limpio

--- Frontend/src/app/locales/interfaces/locales.interface.ts
+++ Frontend/src/app/locales/interfaces/locales.interface.ts
@@ -4,12 +4,4 @@ export interface LocalInterface {
     customers: number;
     pathimagen: string;
     alquilado: boolean;
-
-    // constructor(superficie: number, habilitado: boolean, customers: number, pathimagen: string, alquilado: boolean) {
-    //   this.superficie = superficie;
-    //   this.habilitado = habilitado;
-    //   this.customers = customers;
-    //   this.pathimagen = pathimagen;
-    //   this.alquilado = alquilado;
-    // }
 }
