7b21e56df9aadc0039dde30e5c1a03e179033f77
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-07-09 23:32:33 -0300

    estructura para la edicion de locales

---
+++ Frontend/src/app/locales/pages/localedit/localedit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-localedit',
+  standalone: true,
+  imports: [],
+  templateUrl: './localedit.component.html',
+  styleUrl: './localedit.component.css'
+})
+export class LocaleditComponent {
+
+}
