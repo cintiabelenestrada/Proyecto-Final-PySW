2b05c3d7e5116293a996f57e715874f4b8df2a1f
ezealavar <ezequielalavar2@gmail.com>
2024-07-10 16:50:01 -0300

    Se agrega el componente listado-alquiler con sus archivos CSS, HTML y TS

--- Frontend/src/app/alquiler/models/alquiler.ts
+++ Frontend/src/app/alquiler/models/alquiler.ts
@@ -1,12 +1,14 @@
 import { Local } from "../../local/models/local";
 import { Inquilino } from "../../propietario/models/inquilino";
+import { UsuarioGet } from "../../usuarios/interfaces/usuario-get.interface";
 
 export class Alquiler {
-    _id?: string;
-    inquilino!: Inquilino;
+    _id!: string;
+    inquilino!: UsuarioGet;
     local!: Local;
     plazoMes!: number;
     costoAlquiler!: number;
+    fechaAlquiler!: Date;
     interesAnual!: number;
     activo!: boolean;
 }
---
+++ Frontend/src/app/alquiler/pages/listado-alquiler/listado-alquiler.component.ts
@@ -0,0 +1,96 @@
+import { Component } from '@angular/core';
+import { AlquilerService } from '../../services/alquiler.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
+import { Alquiler } from '../../models/alquiler';
+
+@Component({
+  selector: 'app-listado-alquiler',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './listado-alquiler.component.html',
+  styleUrl: './listado-alquiler.component.css'
+})
+export class ListadoAlquilerComponent {
+  alquileres: Alquiler[] = [];
+  mostrarActivos: boolean = true;
+
+  constructor(
+    private alquilerService: AlquilerService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAlquileresActivos();
+  }
+
+  getAlquileres():void {
+    this.alquilerService.getAlquileres().subscribe(
+      result => {
+        this.alquileres = result;
+        this.alquileres = result.map((element: any) => {
+          let valquiler: Alquiler = new Alquiler();
+          Object.assign(valquiler, element);
+          return valquiler;
+        });
+        this.mostrarActivos = false;
+      }
+    )
+  }
+
+  getAlquileresActivos():void {
+    this.alquilerService.getAlquileresActivos().subscribe(
+      result => {
+        this.alquileres = result.map((element: any) => {
+          let valquiler: Alquiler = new Alquiler();
+          Object.assign(valquiler, element);
+          return valquiler;
+        });
+        this.mostrarActivos = true;
+      }
+    )
+  }
+
+  getAlquileresPorInquilino(inquilinoId: string):void {
+    this.alquilerService.getAlquileresPorInquilino(inquilinoId).subscribe(
+      result => {
+        this.alquileres = result.map((element: any) => {
+          let valquiler: Alquiler = new Alquiler();
+          Object.assign(valquiler, element);
+          return valquiler;
+        });
+        this.mostrarActivos = true;
+      }
+    )
+  }
+
+  registrarAlquiler() {
+    this.router.navigate(['/formulario-alquiler', '0']);
+  }
+
+  modificarAlquiler(alquilerId: string) {
+    this.router.navigate(['/formulario-alquiler', alquilerId]);
+  }
+
+  eliminarAlquiler(alquilerId: string): void {
+    this.alquilerService.deleteAlquiler(alquilerId).subscribe(
+      result => {
+        if(result.status == 1) {
+          this.mostrarActivos ? this.getAlquileresActivos() : this.getAlquileres();
+          this.toastr.warning("Alquiler eliminado con éxito", "Información");
+        }
+      },
+      error => {
+        this.toastr.error('Error al eliminar el alquiler');
+        console.log(error);
+      }
+    )
+  }
+
+  alternarAlquileres() {
+    this.mostrarActivos = !this.mostrarActivos;
+    this.mostrarActivos ? this.getAlquileresActivos() : this.getAlquileres();
+  }
+}
